f6b9d5dcb29721dfbf421e666aaf17490b17b386
Kotresha Patil Thipperudra <68358577+Kotreshpatil@users.noreply.github.com>
2020-09-30 18:15:50 +1300

    Update Raspberry.cs

--- Raspberry.cs
+++ Raspberry.cs
@@ -3,6 +3,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
 using System.IO;
+using Google.Cloud.TextToSpeech.V1;
 using Twilio;
 using Twilio.Clients;
 using Twilio.Rest.Api.V2010.Account;
@@ -10,60 +11,43 @@ using Twilio.Rest.Api.V2010.Account;
 
 namespace sqlconnectionapp
 {
-    public class MainProgram
+    public class Program
     {
         public static void Main(string[] args)
         {
             int i = 0;
             while (i < 4)
             {
-                //This Loop runs every 10 minutes and checks the check flag and set flag is high or no
-                //checkflag = 1 when patient opens pill box atleast once in allocated time and checkflag = 0 when pill box is not opened 
-                //skipflag = 1 when this program checks the check flag and decides to skip this for next iterations
-                int CheckFlag = 0;
-                int SkipFlag = 0;
-                //make query strings for calling method
-                string query1 = "select skip_flag from dbo.flags where row_id = 1";
-                string query2 = "select chk_flg from dbo.flags where row_id = 1";
-                //create an object to call SelectDb method
-                //This method queries db for flags status i.e to check status of CheckFlag and SkipFlag
-                MainProgram d = new MainProgram();
-                SkipFlag = d.SelectDbMethod(query1);
-                CheckFlag = d.SelectDbMethod(query2);
-                if (SkipFlag == 1)
-                {
-                    i = 5;
-                    Environment.Exit(1);
-                    //if skipflag is 1 it skips the iteration run
-                }
-                else
-                {
+                    int CheckFlag = 0;
+                    //int SkipFlag = 0;
+                    //string query1 = "select skip_flag from dbo.flags where row_id = 1";
+                    string query2 = "select chk_flg from dbo.flags where row_id = 1";
+                    Dbcheck d = new Dbcheck();
+                    //SkipFlag = d.SelectDbMethod(query1);
+                    CheckFlag = d.SelectDbMethod(query2);
                     if (CheckFlag == 1)
                     {
-                        //after checking the status of checkflag 
-                        //if checkflag = 1 then it makes switch case for i to decide how many alerts did user made use for consuming the medication
-                        // i = 0 : user takes 0 alerts from the system and consumes medication before the first alert hence the user type = P(Proactive)
-                        // i = 1 : user takes 1 alerts from the system and consumes medication before the first alert hence the user type = R(Reactive)
-                        // i = 2 : user takes 2 alerts from the system and consumes medication before the first alert hence the user type = L(Lazy)
-                        // i = 3 : user takes 3 alerts from the system and consumes medication before the first alert hence the user type = S(SuperLazy)
-                        //and sends a text message if not taken even after 4 alerts
 
-                        MainProgram A = new MainProgram();
+                        DbUpdate A = new DbUpdate();
                         switch (i)
                         {
                             case 0:
-                                A.UpdateDbMethod('P', 1, 0);
+                                A.UpdateDbMethod('P', 0, 0);
+                                Environment.Exit(1);
                                 break;
                             case 1:
-                                A.UpdateDbMethod('R', 1, 0);
+                                A.UpdateDbMethod('R', 0, 0);
+                                Environment.Exit(1);
                                 break;
                             case 2:
-                                A.UpdateDbMethod('L', 1, 0);
+                                A.UpdateDbMethod('L', 0, 0);
+                                Environment.Exit(1);
                                 break;
                             case 3:
-                                //A.UpdateDbMethod('S', 1, 0);
-                                //MainProgram sms = new MainProgram();
-                                //sms.SmsMethod();
+                                A.UpdateDbMethod('S', 0, 0);
+                                Program sms = new Program();
+                                sms.SmsMethod();
+                                Environment.Exit(1);
                                 break;
                         }
                     }
@@ -77,12 +61,11 @@ namespace sqlconnectionapp
                             //Program sms = new Program();
                             //sms.SmsMethod();
                             //break;
-                            Console.WriteLine("Sending SMS for the caretaker");
                         }
+                        
                     }
-                }
                 i++;
-                System.Threading.Thread.Sleep(10);
+                System.Threading.Thread.Sleep(60000);
             }
         }
         public void SmsMethod()
